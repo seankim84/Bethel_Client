9571587659725b53d85df4b49b22c1d2a80dde76
SeanKim <sean.kim84@gmail.com>
2018-12-04 16:08:31 +0700

    AppPresenter with Graphql

--- src/components/App/AppContainer.tsx
+++ src/components/App/AppContainer.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
+import AppPresenter from './AppPresenter';
 import { IS_LOGGED_IN } from './AppQueries';
 
-const AppContainer = ({ data }: any) => <div> {JSON.stringify(data)} </div>; // Queries로 부터의 data
+
+const AppContainer = ({ data }: any) => <AppPresenter isLoggedIn={data.auth.isLoggedIn}/>
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
