05ec024527ec8433e20a3ad224e2737fae1e68de
SeanKim <sean.kim84@gmail.com>
2018-12-04 14:51:07 +0700

    Apollo Setup Part 2

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,13 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { Operation } from 'apollo-boost';
 
 const client = new ApolloClient({
+    request: async(operation: Operation) => { // request를 받기위해 설정
+        operation.setContext({  // operation Object에서 query응답들을 채온다.(모든 operation에 attribute를 설정)
+            header: {
+                "X-JWT": localStorage.getItem('jwt') || ""
+            }
+        });
+    },
     uri: "http:localhost:4000/graphql"
 });
 
