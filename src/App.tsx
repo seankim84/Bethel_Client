ec3f9c79c69cb6d5f5ca5dfbd23ed24a8f6acfc1
SeanKim <sean.kim84@gmail.com>
2018-12-04 14:21:59 +0700

    bethelClient First Confiuration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        Bethel Client Start
+      </div>
+    );
+  }
+}
+
+export default App;
