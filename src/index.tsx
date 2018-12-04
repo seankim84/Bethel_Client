602aa539eadbc59e98d1cfc7f98904e72fa55751
SeanKim <sean.kim84@gmail.com>
2018-12-04 14:45:05 +0700

    Apollo Setup Part 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import ReactDOM from "react-dom";
+import client from "./apollo";
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render( // apollo boost 는 graphql 과 커뮤니케이션 하는 client(grphql을 위한 redux라 생각!
+  <ApolloProvider client={client}> 
+    <App />
+  </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
