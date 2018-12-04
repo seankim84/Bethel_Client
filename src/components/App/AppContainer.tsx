59ca3bf92e27a6ad4707b1af4de7238598303abb
SeanKim <sean.kim84@gmail.com>
2018-12-04 16:20:09 +0700

    Styled-Components TypeScript

--- src/components/App/AppContainer.tsx
+++ src/components/App/AppContainer.tsx
@@ -3,7 +3,6 @@ import { graphql } from 'react-apollo';
 import AppPresenter from './AppPresenter';
 import { IS_LOGGED_IN } from './AppQueries';
 
-
-const AppContainer = ({ data }: any) => <AppPresenter isLoggedIn={data.auth.isLoggedIn}/>
+const AppContainer = ({ data }: any) => <AppPresenter isLoggedIn={ data.auth.isLoggedIn }/> // apollo.ts로 부터 오는 정보이다.
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
