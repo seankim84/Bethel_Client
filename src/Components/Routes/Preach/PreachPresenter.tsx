0c69d78a8dc700aa53ceba6f37bb3187d0b4308a
SeanKim <sean.kim84@gmail.com>
2019-01-01 20:59:42 +0700

    Intro radius

--- src/Components/Routes/Preach/PreachPresenter.tsx
+++ src/Components/Routes/Preach/PreachPresenter.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Helmet from 'react-helmet';
+import Footer from '../../Footer';
 import Header from "../../Header";
 import './Preach.css';
 
@@ -11,25 +12,31 @@ interface IProps {
 class Preach extends React.Component<IProps> {
     public render() {
         return (
-        <section>
-            <Helmet>
-              <title>Bethel | Preach</title>
-            </Helmet>
-            <Header id="HeaderSection" />
-            <div className="preachBody">
-                <div className="preach ">
-                    <div className="preachTitle">
-                        <span>귀 있는 자는 들을지어다 마 / 11:15</span>
+            <div>
+                <section className="Preach">
+                    <Helmet>
+                      <title>Bethel | Preach</title>
+                    </Helmet>
+                    <Header id="" />
+                    <div className="Preachs">
+                        <div className="PreachHeader ">
+                            <div className="PreachExplain">
+                                <h1>Preach</h1>
+                                <p>귀 있는 자는 들을지어다 마 / 11:15</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className ="preach2">
-                    <div onClick={this.props.handleClick} id="preachBox" className="saigon">Saigon Betehl</div>
-                    <div id="preachBox" className="phumyhung">Phu My Hung Bethel</div>
-                    <div id="preachBox" className="airport ">Airport Bethel</div>
-                    <div id="preachBox" className="hanoi">Hanoi Bethel </div>
-                </div>
+                    <div className="PreachBox">
+                        <div id="box" className="PreachBox1">
+                            첫번째 박스
+                        </div>
+                        <div id="box" className="PreachBox1">
+                            두번째 박스
+                        </div>
+                    </div>
+                </section>
+                <Footer />
             </div>
-        </section>
         );
     }
 }
